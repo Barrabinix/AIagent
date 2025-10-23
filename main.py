051941a22ca88a9abb049fa35ca37752c1674305
Barrabinix <barrabino.a@gmail.com>
2025-10-23 22:54:14 +0200

    CH3 - L4 completed. Did not manage this one. I used seer stone...

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ import os
 from google import genai
 from google.genai import types
 from dotenv import load_dotenv
-from functions.get_files_info import schema_get_files_info
-from functions.get_file_content import schema_get_file_content
-from functions.run_python import schema_run_python_file
-from functions.write_file_content import schema_write_file
+from functions.get_files_info import *
+from functions.get_file_content import *
+from functions.run_python import *
+from functions.write_file_content import *
 from prompts import system_prompt
+from call_function import call_function, available_functions
 
 
 def main():
@@ -42,15 +43,6 @@ def main():
 
 def generate_content(client, messages, verbose):
 
-    available_functions = types.Tool(
-        function_declarations=[
-            schema_get_files_info,
-            schema_get_file_content,
-            schema_run_python_file,
-            schema_write_file
-        ]
-    )
-
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
@@ -64,16 +56,29 @@ def generate_content(client, messages, verbose):
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
         print("Response tokens:", response.usage_metadata.candidates_token_count)
 
-    print("Response:")
-    # print(response.text)
+    if not response.function_calls:
+        return response.text
     
     if response.function_calls:
         for function_call_part in response.function_calls:
             print(f"Calling function: {function_call_part.name}({function_call_part.args})")
     else:
         print(response.text)
-    # print(f"Calling function: {response.function_calls.name}({response.args})")
 
+    function_responses = []
+    for function_call_part in response.function_calls:
+        function_call_result = call_function(function_call_part, verbose)
+        if (
+            not function_call_result.parts
+            or not function_call_result.parts[0].function_response
+        ):
+            raise Exception("empty function call result")
+        if verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
+        function_responses.append(function_call_result.parts[0])
 
+    if not function_responses:
+        raise Exception("no function responses generated, exiting.")
+    
 if __name__ == "__main__":
     main()
