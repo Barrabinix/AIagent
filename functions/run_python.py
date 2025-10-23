88aac6e87e94a8636102c79b910482bc3900ac17
Barrabinix <barrabino.a@gmail.com>
2025-10-23 23:04:49 +0200

    CH3 - L4 Corrected after solution

--- functions/run_python.py
+++ functions/run_python.py
@@ -3,27 +3,6 @@ import subprocess
 from google.genai import types
 
 
-def get_files_info(working_directory, directory="."):
-    abs_working_dir = os.path.abspath(working_directory)
-    target_dir = os.path.abspath(os.path.join(working_directory, directory))
-    if not target_dir.startswith(abs_working_dir):
-        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-    if not os.path.isdir(target_dir):
-        return f'Error: "{directory}" is not a directory'
-    try:
-        files_info = []
-        for filename in os.listdir(target_dir):
-            filepath = os.path.join(target_dir, filename)
-            file_size = 0
-            is_dir = os.path.isdir(filepath)
-            file_size = os.path.getsize(filepath)
-            files_info.append(
-                f"- {filename}: file_size={file_size} bytes, is_dir={is_dir}"
-            )
-        return "\n".join(files_info)
-    except Exception as e:
-        return f"Error listing files: {e}"
-
 def run_python_file(working_directory, file_path, args=None):
     abs_working_dir = os.path.abspath(working_directory)
     abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
@@ -60,14 +39,23 @@ def run_python_file(working_directory, file_path, args=None):
 
 schema_run_python_file = types.FunctionDeclaration(
     name="run_python_file",
-    description="Executes a specified Python file and returns its output, constrained to the working directory.",
+    description="Executes a Python file within the working directory and returns the output from the interpreter.",
     parameters=types.Schema(
         type=types.Type.OBJECT,
         properties={
             "file_path": types.Schema(
                 type=types.Type.STRING,
-                description="The path to the Python file to execute, relative to the working directory.",
+                description="Path to the Python file to execute, relative to the working directory.",
+            ),
+            "args": types.Schema(
+                type=types.Type.ARRAY,
+                items=types.Schema(
+                    type=types.Type.STRING,
+                    description="Optional arguments to pass to the Python file.",
+                ),
+                description="Optional arguments to pass to the Python file.",
             ),
         },
+        required=["file_path"],
     ),
 )
